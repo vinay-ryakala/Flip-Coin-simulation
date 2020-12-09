ef2c1bbee9f092e79de7e3e215a41239a9ccd65e
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-09 13:48:10 +0530

    Adding code to determine heads or tails using RANDOM

--- flipCionSimulation.sh
+++ flipCionSimulation.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
 echo "welcome to Flip Coin Simulation"
+
+HEADS=1
+TAILS=2
+
+simulationResult=$((RANDOM%2))
+
+if [ $simulationResult -eq $HEADS ]
+then
+		echo "Heads is winner"
+else
+		echo "Tails is winner"
+fi
