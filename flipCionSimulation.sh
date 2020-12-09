066b72b34ccad8313fe7f39901c2150db3d00f27
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-09 14:14:20 +0530

    Refactoring programm to run multiple time UC2

--- flipCionSimulation.sh
+++ flipCionSimulation.sh
@@ -5,11 +5,22 @@ echo "welcome to Flip Coin Simulation"
 HEADS=1
 TAILS=2
 
-simulationResult=$((RANDOM%2))
+read -p "give  number of times filp coin Simulation should run:" number
+headcount=0
+tailcount=0
+counter=0
+while [ $counter -lt $number ]
+do
+	simulationResult=$((RANDOM%2))
 
-if [ $simulationResult -eq $HEADS ]
-then
-		echo "Heads is winner"
-else
-		echo "Tails is winner"
-fi
+	if [ $simulationResult -eq $HEADS ]
+	then
+		((headcount++))
+	else
+		((tailcount++))
+	fi
+	((counter++))
+done
+
+echo "$headcount times head won"
+echo "$tailcount times tail won"
