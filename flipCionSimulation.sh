b505b5675b192741da0be3b3e71929e73eb9e32c
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-11 20:32:58 +0530

    Refactoring code get minimum difference grater than 2 If it is a tie UC4

--- flipCionSimulation.sh
+++ flipCionSimulation.sh
@@ -8,8 +8,9 @@ TAILS=2
 headcount=0
 tailcount=0
 counter=0
+numberOfTimes=22
 
-while [ $counter != 21 ]
+while [ $counter -le $numberOfTimes ]
 do
 	simulationResult=$((RANDOM%2))
 
@@ -19,23 +20,20 @@ do
 	else
 		((tailcount++))
 	fi
-	if [ $headcount -gt $tailcount ]
+	diff=$headcount-$tailcount
+	if [[ $diff -eq 0 && $counter -eq $numberOfTimes ]]
 	then
-		counter=$headcount
-	else
-		counter=$tailcount
+		counter=$diff
 	fi
+	
+	((counter++))
 done
 
 if [ $headcount -gt $tailcount ]
 then   
 	diff=$(($headcount-$tailcount))
 	echo "heads won by $diff"
-elif [ $headcount -lt $tailcount ]
-then
+else
 	diff=$(($tailcount-$headcount))
 	echo "tails won by $diff"
-else
-	echo "Its a tie"
 fi
-
